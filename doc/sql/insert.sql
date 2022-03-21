65ff53461eefd8a6f859f5980dd01a668f3c9336
zty-f <2559247260@qq.com>
2022-03-21 21:17:16 +0800

    2022.3.21 21:16 插入测试数据

---
+++ doc/sql/insert.sql
@@ -0,0 +1,8 @@
+insert into `t_goods`
+    values
+    (1,'XiaoMi 12','XIAOMI12 6+128','/img/xiaomi12.png','xiaomi12!!!!!!!','3699.00',100),
+    (2,'XiaoMi 12Pro','XIAOMI12 8+128','/img/xiaomi12pro.png','xiaomi12pro!!!!!!!','4999.00',100);
+
+insert into `t_seckill_goods`
+values (1,1,'3299',20,'2022-3-21 08:00:00','2022-3-21 09:00:00'),
+       (2,2,'4599',20,'2022-3-21 08:00:00','2022-3-21 09:00:00');
