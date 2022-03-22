8bdbef1df4cff32dd657f047395da587029389a5
zty-f <2559247260@qq.com>
2022-03-22 15:45:54 +0800

    2022.3.22 15:44 bug：静态资源访问失败解决
    Signed-off-by: zty-f <2559247260@qq.com>

--- src/main/java/com/zty/seckill/config/WebConfig.java
+++ src/main/java/com/zty/seckill/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.List;
  * @Description: mvc配置类
  */
 @Configuration
-@EnableWebMvc
+@EnableWebMvc  //开启这个注解表示全局接管mvc默认配置而使用这个类的配置
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -25,5 +26,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userArgumentResolver);
+    }
+
+    //约定大于配置，此处需要自己配置mvc静态资源地址
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
