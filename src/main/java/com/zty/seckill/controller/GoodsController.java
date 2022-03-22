b3d3c660c7b2234506029a3af6f8d22d56ac1e67
zty-f <2559247260@qq.com>
2022-03-22 17:30:33 +0800

    2022.3.22 17:29 秒杀时间倒计时完成

--- src/main/java/com/zty/seckill/controller/GoodsController.java
+++ src/main/java/com/zty/seckill/controller/GoodsController.java
@@ -3,11 +3,14 @@ package com.zty.seckill.controller;
 import com.zty.seckill.pojo.User;
 import com.zty.seckill.service.IGoodsService;
 import com.zty.seckill.service.IUserService;
+import com.zty.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 /**
  * @version V1.0
@@ -57,7 +60,27 @@ public class GoodsController {
     @RequestMapping("/toDetail/{goodsId}")
     public String toDetail(Model model,User user,@PathVariable Long goodsId){
         model.addAttribute("user",user);
-        model.addAttribute("goods",goodsService.findGoodsVoByGoodsId(goodsId));
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态
+        int secKillStatus = 0;
+        //秒杀倒计时
+        int remainSeconds = 0;
+        //秒杀还未开始
+        if (nowDate.before(startDate)) {
+            remainSeconds = (int) ((startDate.getTime() - nowDate.getTime())/1000);
+        }else if(nowDate.after(endDate)){ //秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        }else { //秒杀进行中
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("remainSeconds",remainSeconds);
+        model.addAttribute("secKillStatus",secKillStatus);
+        model.addAttribute("goods",goodsVo);
         return "goodsDetail";
     }
 }
