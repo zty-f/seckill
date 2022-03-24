93c730f1def261424597916e2d153acc96f6b433
zty-f <2559247260@qq.com>
2022-03-24 15:44:36 +0800

    2022.3.24 15:44 添加页面缓存到Redis

--- src/main/java/com/zty/seckill/controller/GoodsController.java
+++ src/main/java/com/zty/seckill/controller/GoodsController.java
@@ -5,12 +5,21 @@ import com.zty.seckill.service.IGoodsService;
 import com.zty.seckill.service.IUserService;
 import com.zty.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @version V1.0
@@ -29,6 +38,12 @@ public class GoodsController {
     @Autowired
     private IGoodsService goodsService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private ThymeleafViewResolver thymeleafViewResolver;
+
     /**
      * windows 优化前 QPS：1245
      *
@@ -39,11 +54,25 @@ public class GoodsController {
      * @version V1.0
      * @Description: 跳转到商品列表页面
      */
-    @RequestMapping("/toList")
-    public String toList(Model model,User user){
+    @RequestMapping(value = "/toList",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String toList(Model model, User user,
+                         HttpServletRequest request, HttpServletResponse response){
+        //Redis中获取页面，如果不为空，直接返回页面
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsList");
+        if(!StringUtils.isEmpty(html)){
+            return html;
+        }
         model.addAttribute("user",user);
         model.addAttribute("goodsList",goodsService.findGoodsVo());
-        return "goodsList";
+        //如果redis中html为空，手动渲染页面，然后缓存在redis中并返回
+        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsList", webContext);
+        if(!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsList",html,60, TimeUnit.SECONDS);
+        }
+        return html;
     }
 
 
@@ -59,8 +88,16 @@ public class GoodsController {
      * @author: zty-f
      * @date:  2022-03-22 16:09
      */
-    @RequestMapping("/toDetail/{goodsId}")
-    public String toDetail(Model model,User user,@PathVariable Long goodsId){
+    @RequestMapping(value = "/toDetail/{goodsId}",produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String toDetail(Model model,User user,@PathVariable Long goodsId,
+                           HttpServletRequest request, HttpServletResponse response){
+        //Redis中获取页面，如果不为空，直接返回页面
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String html = (String) valueOperations.get("goodsDetail"+goodsId);
+        if(!StringUtils.isEmpty(html)){
+            return html;
+        }
         model.addAttribute("user",user);
         GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
         Date startDate = goodsVo.getStartDate();
@@ -83,6 +120,12 @@ public class GoodsController {
         model.addAttribute("remainSeconds",remainSeconds);
         model.addAttribute("secKillStatus",secKillStatus);
         model.addAttribute("goods",goodsVo);
-        return "goodsDetail";
+        //如果redis中html为空，手动渲染页面，然后缓存在redis中并返回
+        WebContext webContext = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goodsDetail", webContext);
+        if(!StringUtils.isEmpty(html)){
+            valueOperations.set("goodsDetail"+goodsId,html,60, TimeUnit.SECONDS);
+        }
+        return html;
     }
 }
